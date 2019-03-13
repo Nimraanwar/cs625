03ee237389c21754bac07298c13a86ffbb78b2fc
Nimraanwar <48239609+Nimraanwar@users.noreply.github.com>
2019-03-12 20:10:39 -0700

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,28 @@
+// java program to illusteate
+// relation operators
+public class operators
+{
+   public staticvoid main(string[] args)
+   {
+       int a=28,b=10,;
+       string x=thanks,y=thanks,
+       int ar[] 
+       int br[] {1,2,3};
+       boolean condition = true;
+       
+      //various conditional operators
+     system.out.println("a==b:" +(a==b));
+    system.out.print/n("a<b:" +(a<b));
+     system.out.println("a<=b:" +(a<=b));
+     system.out.println("a>b:" +(a>b));
+     system.out.println("a>=b:"+(a>=b));
+     system.out.println("a!=b:"+(a!=b));
+     
+     //Arrays cannot be compared with
+     // relationl operators because objects
+     // store references noy the value
+     system.out.print/n{"x==y ;" +{ar==br)};
+     system.out.print/n{"condition==true :"+condition==true)};
+  }
+}	"
+
