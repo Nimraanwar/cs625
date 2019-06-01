164bf025bb74bec3159e29d26ac52869321d60fe
Nimraanwar <48239609+Nimraanwar@users.noreply.github.com>
2019-05-31 19:42:59 -0700

    inches java

---
+++ Inches.java
@@ -0,0 +1,21 @@
+/*
+  compute the number of cubic incches
+  in 1 cubic mile
+*/
+  class Inches
+{
+   public statics void main(string args[])
+[
+   long cI;
+   LONG Im;
+
+    Im=5280* 12;
+
+    cI=im* im* im;
+  
+   system.out.println("There are " +ci +" cubic inches in cubic mile.");
+   
+
+
+    }
+}
