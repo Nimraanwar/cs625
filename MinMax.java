207017e934cf1d12c50dd56b5febd04f41368fc8
Nimraanwar <48239609+Nimraanwar@users.noreply.github.com>
2019-05-31 19:39:01 -0700

    minmax example

---
+++ MinMax.java
@@ -0,0 +1,21 @@
+class MInMax2 {
+public static void main(string args [] } {
+int nums []= {99,-10,100123,18,-978,5623,463,-9,287,49}:
+
+int min, max;
+ min=max=nums[0];+
+for(int i=1;i<10;i++) {
+if (nums[i] <min)  min=nums[i];
+if (nums [i]>max)  max=nums[i];
+  }
+  system.out.println("Min and max: " + min + " "+max); 
+ }
+}
+Demonstrate an array overrun.
+public static void main(string args {}) {
+int sample [] = new int [10];
+int i;
+for(i=0;i,100; i=i+1)
+sample[i] = i;
+}
+}
